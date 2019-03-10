8bd37632a4c9fabd91a64849095752b0f9afdd6a
Manisha <mgoyel.2709@gmail.com>
2019-03-11 00:16:30 +0530

    Update models.py
    updated

--- models.py
+++ models.py
@@ -1,17 +1,45 @@
 from app import db
 
-class Book(db.Model):
-    __tablename__ = 'books'
+
+class Holiday(db.Model):
+    __tablename__ = 'holidays'
+
+   
+
+    id = db.Column(db.Integer, primary_key=True)
+    month = db.Column(db.String())
+    date = db.Column(db.String())
+    event = db.Column(db.String())
+
+    def __init__(self, month, date, event):
+        self.month = month
+        self.date = date
+        self.event = event
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'month': self.month,
+            'date': self.date,
+            'event':self.event
+        }
+
+    __tablename__ = 'stu_info'
+
+   
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
-    author = db.Column(db.String())
-    published = db.Column(db.String())
+    address = db.Column(db.String())
+    city = db.Column(db.String())
 
-    def __init__(self, name, author, published):
+    def __init__(self, name, address, city):
         self.name = name
-        self.author = author
-        self.published = published
+        self.address = address
+        self.city = city
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -20,6 +48,33 @@ class Book(db.Model):
         return {
             'id': self.id, 
             'name': self.name,
-            'author': self.author,
-            'published':self.published
+            'address': self.address,
+            'city':self.city
+        }
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    month = db.Column(db.String())
+    date = db.Column(db.String())
+    event = db.Column(db.String())
+
+    def __init__(self, month, date, event):
+        self.month = month
+        self.date = date
+        self.event = event
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'month': self.month,
+            'date': self.date,
+            'event':self.event
         }
