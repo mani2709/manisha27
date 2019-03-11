e647ac6dc92a22d15bfc532ff689224132642979
mani2709 <45124886+mani2709@users.noreply.github.com>
2019-03-11 14:00:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,8 +100,8 @@ def get_data():
         All_Holidays=Holiday.query.all()
         All_Students=Student_Info.query.all()
         for row in All_Students:
-            print("All Students name -",row["name"])
-            print("All Students city -",row["city"])
+            print("All Students name -",row.name)
+            print("All Students city -",row.city)
         return render_template("list.html",All_Holidays = All_Holidays,All_Students = All_Students)
 
         #return  jsonify([e.serialize() for e in books])
