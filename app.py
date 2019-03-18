1a2eb28574d0d968ceb0c07f832270c7c95ebfa6
Manisha <mgoyel.2709@gmail.com>
2019-03-19 01:20:49 +0530

    aa

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Student_Info
 from models import Holiday
 
 #from models import Holiday
@@ -46,13 +45,62 @@ def get_all():
     except Exception as e:
         return(str(e))
 
-@app.route("/get/<month_>" )
-def get_by_id(month_):
+@app.route("/get",methods=['GET', 'POST'] )
+def get_by_id():
+    print("helloooo")
+
+    req = request.get_json(silent=True, force=True)
+    action = req['queryResult']['parameters']['function']
+    month = req['queryResult']['parameters']['Months']
+    print("action is", action)
+    print("month is", month)
+    try: 
+        if action=='Holiday':
+            holiday=Holiday.query.filter_by(month=month).all()
+            holiday_count=Holiday.query.filter_by(month=month).count()
+            print("count the holidays",holiday_count, len(holiday))
+
+            #print("Month is",row.month)
+            #print("Date is",holiday.date)
+            #print("Event is",holiday.event)
+            if(len(holiday)==0):
+                 response =  """
+
+                        {0}
+                    
+                        """.format("There are no holidays in month of "+ month)
+                 reply = {"fulfillmentText": response}
+                 print("hi there")
+                 return jsonify(reply)
+            i = 0
+            Result=''
+            response=''
+            reply= ''
+            for row in holiday:
+
+                i = i + 1
+                print("print rows", row.id, row.month, row.date, row.event)
+                Result= str(row.id) +'There is a holiday in the month of '+ str(row.month) + ' on'+str(row.date) + 'for the occasion of' + str(row.event) + ' ', 
+           # Result= 'Dear candidate there is one holiday in the month of {0}'.format(holiday.month)
+
+                print("result is", Result)
+                response = response + """
+
+                        {0}
+                    
+                        """.format(Result,)
+                
+                reply = {"fulfillmentText": response,}
+
+            return jsonify(reply)
+        else:
+
     
-    try:
-        holiday=Holiday.query.filter_by(month=month_).first()
-        print("print rows", holiday)
-        return jsonify(holiday.serialize())
+            response =  """
+                    Response : {0}
+                    """.format("action is not valid")
+            reply = {"fulfillmentText": response,}
+        #return jsonify(holiday.serialize())
     except Exception as e:
         return(str(e))
 
@@ -94,36 +142,87 @@ def add_student_info():
             return(str(e))
     return render_template("studentdata.html")
 
+
 @app.route("/getdata")
+
 def get_data():
+
     try:
+
         All_Holidays=Holiday.query.all()
+
         All_Students=Student_Info.query.all()
+
         for row in All_Students:
+
             print("All Students name -",row.name)
+
             print("All Students city -",row.city)
+
         return render_template("list.html",All_Holidays = All_Holidays,All_Students = All_Students)
 
+
+
         #return  jsonify([e.serialize() for e in books])
+
     except Exception as e:
+
         return(str(e))
+
+
+
 
 
 @app.route("/get/<name_>" )
+
 def get_by_name(name_):
+
     #req = request.get_json(silent=True, force=True)
+
     #print("in comin grequest",req)
+
     #action = req['queryResult']['parameters']['Holiday']
+
     #month = req['queryResult']['parameters']['Months']
+
     try:
+
         table=Holiday.query.filter_by(name=name_).first()
+
         print("print rows", table)
+
         return (table)
+
         #return jsonify(table.serialize())
+
     except Exception as e:
+
         return(str(e))
 
 
+
+@app.route("/add/ex_schedule",methods=['GET', 'POST'])
+def add_exam_schedule():
+    if request.method == 'POST':
+        course=request.form.get('course')
+        semester=request.form.get('semester')
+        date=request.form.get('date')
+        sub_code=request.form.get('sub_code')
+        subject=request.form.get('subject')
+        try:
+            schedule=Schedule(
+                course=course,
+                semester=semester,
+                date=date,
+                sub_code=sub_code,
+                subject=subject
+            )
+            db.session.add(schedule)
+            db.session.commit()
+            return "schedule added. schedule id={}".format(schedule.id)
+        except Exception as e:
+            return(str(e))
+    return render_template("getschedule.html")
 
 if __name__ == '__main__':
     app.run()
