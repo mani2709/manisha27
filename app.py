211a8d14e4d459428fed915cd85d4b8fe22e75ec
Manisha <mgoyel.2709@gmail.com>
2019-03-30 12:14:18 +0530

    ee

--- app.py
+++ app.py
@@ -73,10 +73,10 @@ def get_by_id():
     # end   =datetime.strptime(request.vars.Expected_Possession_Date,"%Y-%m-%d").date()
     try: 
         if action=='Holiday':
-            holiday=Holiday.query.filter_by(start_end.strftime("%B") == month).all()
+            holiday=Holiday.query.filter_by(start_datestrftime("%B") == month).all()
             
-            holiday_count=Holiday.query.filter_by(month=month).count()
-            print("count the holidays",holiday_count, len(holiday))
+            #holiday_count=Holiday.query.filter_by(month=month).count()
+            #print("count the holidays",holiday_count, len(holiday))
 
             #print("Month is",row.month)
             #print("Date is",holiday.date)
