1deb24931c511296395808d43a89152c7e2e2dee
Manisha <mgoyel.2709@gmail.com>
2019-04-27 00:19:27 +0530

    kmfkfmgkf

--- models.py
+++ models.py
@@ -183,36 +183,6 @@ class Timetable(db.Model):
             
         }
 
-class Calendar(db.Model):
-    __tablename__ = 'calendar'
-
-   
-
-    id = db.Column(db.Integer, primary_key=True)
-    
-    month =  db.Column(db.String())
-
-    date =db.Column(db.String())
-
-    event = db.Column(db.String())
-
-    def __init__(self, month, date, event):
-        
-        self.month = month
-        self.date = date
-        self.event = event
-
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
-
-    def serialize(self):
-        return {
-            'id': self.id, 
-        
-            'month': self.month,
-            'date': self.date,
-            'event':self.event
-        }
 
 
 
